f32f62fa0faa3ccd5f3554bb0688bc98e2590008
Max <borshishe@gmail.com>
2021-02-25 22:17:56 +0300

    .

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset = new Vector3(0, 0, -10);
+
+    private void Start()
+    {
+        player = GameObject.Find("Player").transform;
+    }
+
+    void Update()
+    {
+        transform.position = player.position+offset;
+    }
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.AssetImporters;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float force;
+    public float gravity;
+    public int turn;
+    public float fuel;
+    public bool isGrounded;
+    void Start()
+    {
+        force = 1000;
+        gravity = 300;
+        turn = 90;
+        fuel = 10;
+        rb = transform.GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(0, -gravity*Time.fixedDeltaTime,0);
+        if (Input.GetKey("space") && fuel>0)
+        {
+            rb.AddRelativeForce(0, force * Time.fixedDeltaTime, 0);
+            fuel -= 4 * Time.deltaTime;
+        }
+
+        if (Input.GetKey("d")&&isGrounded==false)
+        {
+            transform.Rotate(0,0,-turn*Time.fixedDeltaTime);
+        }
+        if (Input.GetKey("a")&&isGrounded==false)
+        {
+            transform.Rotate(0,0,turn*Time.fixedDeltaTime);
+        }
+    }
+
+    private void Update()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down,  0.8f))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+}
